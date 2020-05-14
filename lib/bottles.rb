2c24c9d97253a19b0e5451fbf7e5156e2c3c0f0d
Sandi Metz <sandi@sandimetz.com>
2020-05-14 11:42:59 -0400

    pass verses test #1 w/literal

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -23,4 +23,16 @@ class Bottles
       "#{number-1} bottles of beer on the wall.\n"
     end
   end
+
+  def verses(_, _)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n" +
+    "\n" +
+    "98 bottles of beer on the wall, " +
+    "98 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "97 bottles of beer on the wall.\n"
+  end
 end
