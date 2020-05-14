b62d5b8cf1e0334154167d197af03520de7e75f7
Sandi Metz <sandi@sandimetz.com>
2020-05-14 15:24:37 -0400

    revert case 6/7

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,16 +24,6 @@ class Bottles
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
-    when 6
-      "1 six-pack of beer on the wall, " +
-      "1 six-pack of beer.\n" +
-      "Take one down and pass it around, " +
-      "5 bottles of beer on the wall.\n"
-    when 7
-      "7 bottles of beer on the wall, " +
-      "7 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 six-pack of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
