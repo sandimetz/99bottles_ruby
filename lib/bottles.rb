82fa5532b02d5e763bd03d0965523d3fe8711c3b
Sandi Metz <sandi@sandimetz.com>
2020-05-14 16:57:59 -0400

    remove obsolete to_s's

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,12 +10,12 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).to_s.capitalize} bottles of beer on the wall, " +
+      "#{quantity(number).capitalize} bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
-      "#{quantity(number).to_s.capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
