6fd68644d2f86c167160bd148329a9c628bf0e74
Sandi Metz <sandi@sandimetz.com>
2020-05-15 11:27:34 -0400

    use bottle_number is phrase 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} " +
       "of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{quantity(successor(number))} #{container(successor(number))} " +
       "of beer on the wall.\n"
