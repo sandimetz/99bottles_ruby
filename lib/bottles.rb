93b06c7a88ba74901f4dc27203901dc469830284
Sandi Metz <sandi@sandimetz.com>
2020-05-15 11:30:21 -0400

    delete obsolete code

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,22 +18,6 @@ class Bottles
     "#{next_bottle_number.quantity} #{next_bottle_number.container} " +
       "of beer on the wall.\n"
   end
-
-  def quantity(number)
-    BottleNumber.new(number).quantity
-  end
-
-  def container(number)
-    BottleNumber.new(number).container
-  end
-
-  def action(number)
-    BottleNumber.new(number).action
-  end
-
-  def successor(number)
-    BottleNumber.new(number).successor
-  end
 end
 
 class BottleNumber
