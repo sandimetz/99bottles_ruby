sandimetz/99bottles_ruby_milk

send #successor in else phrase 4

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +
       "#{action(number)}, " +
-      "#{quantity(number-1)} #{container(number-1)} of milk on the wall.\n"
+      "#{quantity(successor(number))} #{container(number-1)} of milk on the wall.\n"
     end
   end
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
     end
   end
 