sandimetz/99bottles_ruby_milk

send #quantity(99)

lib/bottles.rb
@@ -13,7 +13,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +
       "#{action(number)}, " +
-      "99 bottles of milk on the wall.\n"
+      "#{quantity(99)} bottles of milk on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -13,7 +13,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "99 bottles of beer on the wall.\n"
+      "#{quantity(99)} bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +