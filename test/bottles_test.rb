sandimetz/99bottles_ruby_milk

copy 1st test from BottlesTest to BottleVerseTest

test/bottles_test.rb
@@ -4,6 +4,14 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 
 class BottleVerseTest < Minitest::Test
+  def test_the_first_verse
+    expected =
+      "99 bottles of milk on the wall, " +
+      "99 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(99)
+  end
 end
 
 class BottlesTest < Minitest::Test

sandimetz/99bottles_ruby

test/bottles_test.rb
@@ -4,6 +4,14 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 
 class BottleVerseTest < Minitest::Test
+  def test_the_first_verse
+    expected =
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(99)
+  end
 end
 
 class BottlesTest < Minitest::Test