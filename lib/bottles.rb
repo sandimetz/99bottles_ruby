sandimetz/99bottles_ruby_milk

hack to_s in else case to sidestep error

lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     else
-      "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
+      "#{quantity(number).to_s.capitalize} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number).to_s.capitalize} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"