sandimetz/99bottles_ruby_milk

try common template

lib/bottles.rb
@@ -20,6 +20,10 @@ class Bottles
       "#{action(number)}, " +
       "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
     end
+    "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
+    "#{quantity(number)} #{container(number)} of milk.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
   end
 
   def quantity(number)

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -20,6 +20,10 @@ class Bottles
       "#{action(number)}, " +
       "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
     end
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
   def quantity(number)