sandimetz/99bottles_ruby_milk

1 case sends quantity

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{number} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of milk on the wall.\n"
+      "#{quantity} bottles of milk on the wall.\n"
     else
       "#{number} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "#{quantity} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +