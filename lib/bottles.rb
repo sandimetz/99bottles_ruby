sandimetz/99bottles_ruby_milk

consolidate phrase 4 strings

lib/bottles.rb
@@ -14,7 +14,7 @@ class Bottles
     "#{bottle_number} of milk on the wall, ".capitalize +
     "#{bottle_number} of milk.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} " + "of milk on the wall.\n"
+    "#{next_bottle_number} of milk on the wall.\n"
   end
 end
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -14,7 +14,7 @@ class Bottles
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} " + "of beer on the wall.\n"
+    "#{next_bottle_number} of beer on the wall.\n"
   end
 end
 