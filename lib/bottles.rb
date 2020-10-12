sandimetz/99bottles_ruby_milk

remove obsolete to_s's

lib/bottles.rb
@@ -10,12 +10,12 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).to_s.capitalize} bottles of milk on the wall, " +
+      "#{quantity(number).capitalize} bottles of milk on the wall, " +
       "no more bottles of milk.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     else
-      "#{quantity(number).to_s.capitalize} #{container(number)} of milk on the wall, " +
+      "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -10,12 +10,12 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).to_s.capitalize} bottles of beer on the wall, " +
+      "#{quantity(number).capitalize} bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
-      "#{quantity(number).to_s.capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"