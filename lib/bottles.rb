sandimetz/99bottles_ruby_milk

send quantity(arg) in else  phrase 4

lib/bottles.rb
@@ -23,7 +23,7 @@ class Bottles
       "#{number} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{number-1} #{container(number-1)} of milk on the wall.\n"
+      "#{quantity(number-1)} #{container(number-1)} of milk on the wall.\n"
     end
   end
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -23,7 +23,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{number-1} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
   end
 