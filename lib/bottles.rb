sandimetz/99bottles_ruby_milk

make case 0 phrase 2 like else phrase 2

lib/bottles.rb
@@ -11,7 +11,7 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
-      "no more bottles of milk.\n" +
+      "#{quantity(number)} #{container(number)} of milk.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     else

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -11,7 +11,7 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "no more bottles of beer.\n" +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else