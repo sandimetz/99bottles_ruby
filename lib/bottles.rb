sandimetz/99bottles_ruby_milk

add case 6/7 to verse

lib/bottles.rb
@@ -24,6 +24,16 @@ class Bottles
       "2 bottles of milk.\n" +
       "Take one down and pass it around, " +
       "1 bottle of milk on the wall.\n"
+    when 6
+      "1 six-pack of milk on the wall, " +
+      "1 six-pack of milk.\n" +
+      "Take one down and pass it around, " +
+      "5 bottles of milk on the wall.\n"
+    when 7
+      "7 bottles of milk on the wall, " +
+      "7 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of milk on the wall.\n"
     else
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -24,6 +24,16 @@ class Bottles
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
+    when 6
+      "1 six-pack of beer on the wall, " +
+      "1 six-pack of beer.\n" +
+      "Take one down and pass it around, " +
+      "5 bottles of beer on the wall.\n"
+    when 7
+      "7 bottles of beer on the wall, " +
+      "7 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +