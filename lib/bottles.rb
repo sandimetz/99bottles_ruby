sandimetz/99bottles_ruby_milk

pass verse 99 test

lib/bottles.rb
@@ -1,4 +1,8 @@
 class Bottles
   def verse(_)
+    "99 bottles of milk on the wall, " +
+    "99 bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of milk on the wall.\n"
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,4 +1,8 @@
 class Bottles
   def verse(_)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
   end
 end