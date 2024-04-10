sandimetz/99bottles_ruby_milk

use successor everywhere

lib/bottles.rb
@@ -13,12 +13,12 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +
       "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of milk on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +
       "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of milk on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
     end
   end
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -13,12 +13,12 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
     end
   end
 