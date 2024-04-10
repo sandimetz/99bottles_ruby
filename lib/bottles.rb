sandimetz/99bottles_ruby_milk

separate conditional from lyrics

lib/bottles.rb
@@ -1,15 +1,14 @@
 class Bottles
   def verse(number)
     if number == 99
-      "99 bottles of milk on the wall, " +
-      "99 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of milk on the wall.\n"
+      n = 99
     else
-      "3 bottles of milk on the wall, " +
-      "3 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of milk on the wall.\n"
+      n = 3
     end
+
+    "#{n} bottles of milk on the wall, " +
+    "#{n} bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "#{n - 1} bottles of milk on the wall.\n"
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,15 +1,14 @@
 class Bottles
   def verse(number)
     if number == 99
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
+      n = 99
     else
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
+      n = 3
     end
+
+    "#{n} bottles of beer on the wall, " +
+    "#{n} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{n - 1} bottles of beer on the wall.\n"
   end
 end