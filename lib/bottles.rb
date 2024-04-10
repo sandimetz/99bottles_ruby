sandimetz/99bottles_ruby_milk

send quantity(arg) in case 1 phrase 4

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{number} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{quantity} bottles of milk on the wall.\n"
+      "#{quantity(number-1)} bottles of milk on the wall.\n"
     else
       "#{number} #{container(number)} of milk on the wall, " +
       "#{number} #{container(number)} of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{quantity} bottles of beer on the wall.\n"
+      "#{quantity(number-1)} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +