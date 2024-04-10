sandimetz/99bottles_ruby_milk

add verse 7 test

test/bottles_test.rb
@@ -22,6 +22,15 @@ class BottleVerseTest < Minitest::Test
     assert_equal expected, BottleVerse.lyrics(3)
   end
 
+  def test_verse_7
+    expected =
+      "7 bottles of milk on the wall, " +
+      "7 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of milk on the wall.\n"
+    assert_equal expected, BottleVerse.lyrics(7)
+  end
+
   def test_verse_2
     expected =
       "2 bottles of milk on the wall, " +

sandimetz/99bottles_ruby

test/bottles_test.rb
@@ -22,6 +22,15 @@ class BottleVerseTest < Minitest::Test
     assert_equal expected, BottleVerse.lyrics(3)
   end
 
+  def test_verse_7
+    expected =
+      "7 bottles of beer on the wall, " +
+      "7 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of beer on the wall.\n"
+    assert_equal expected, BottleVerse.lyrics(7)
+  end
+
   def test_verse_2
     expected =
       "2 bottles of beer on the wall, " +