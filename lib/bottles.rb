sandimetz/99bottles_ruby_milk

consolidate phrase 1 strings

lib/bottles.rb
@@ -11,7 +11,7 @@ class Bottles
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number} ".capitalize + "of milk on the wall, " +
+    "#{bottle_number} of milk on the wall, ".capitalize +
     "#{bottle_number} of milk.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number} " +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -11,7 +11,7 @@ class Bottles
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number} ".capitalize + "of beer on the wall, " +
+    "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number} " +