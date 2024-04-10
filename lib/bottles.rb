sandimetz/99bottles_ruby_milk

unite phrase 1 on single line

lib/bottles.rb
@@ -11,8 +11,7 @@ class Bottles
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number} ".capitalize +
-      "of milk on the wall, " +
+    "#{bottle_number} ".capitalize + "of milk on the wall, " +
     "#{bottle_number} of milk.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number} " +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -11,8 +11,7 @@ class Bottles
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number} ".capitalize +
-      "of beer on the wall, " +
+    "#{bottle_number} ".capitalize + "of beer on the wall, " +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number} " +