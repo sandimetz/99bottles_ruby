sandimetz/99bottles_ruby_milk

Add shameless green example

lib/bottles.rb
@@ -3,85 +3,32 @@ class Bottles
     verses(99, 0)
   end
 
-  def verses(bottles_at_start, bottles_at_end)
-    bottles_at_start.downto(bottles_at_end).map do |bottles|
-      verse(bottles)
-    end.join("\n")
+  def verses(upper, lower)
+    upper.downto(lower).map {|i| verse(i)}.join("\n")
   end
 
-  def verse(bottles)
-    Round.new(bottles).to_s
-  end
-end
-
-class Round
-  attr_reader :bottles
-  def initialize(bottles)
-    @bottles = bottles
-  end
-
-  def to_s
-    challenge + response
-  end
-
-  def challenge
-    bottles_of_milk.capitalize + " " + on_wall + ", " +
-    bottles_of_milk + ".\n"
-  end
-
-  def response
-    go_to_the_store_or_take_one_down + ", " +
-    bottles_of_milk + " " + on_wall + ".\n"
-  end
-
-  def bottles_of_milk
-    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{milk}"
-  end
-
-  def milk
-    "milk"
-  end
-
-  def on_wall
-    "on the wall"
-  end
-
-  def pluralized_bottle_form
-    last_milk? ? "bottle" : "bottles"
-  end
-
-  def anglicized_bottle_count
-    all_out? ? "no more" : bottles.to_s
-  end
-
-  def go_to_the_store_or_take_one_down
-    if all_out?
-      @bottles = 99
-      buy_new_milk
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of milk on the wall, " +
+      "no more bottles of milk.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of milk on the wall.\n"
+    when 1
+      "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n"
+    when 2
+      "2 bottles of milk on the wall, " +
+      "2 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of milk on the wall.\n"
     else
-      lyrics = drink_milk
-      @bottles -= 1
-      lyrics
+      "#{number} bottles of milk on the wall, " +
+      "#{number} bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of milk on the wall.\n"
     end
-  end
-
-  def buy_new_milk
-    "Go to the store and buy some more"
-  end
-
-  def drink_milk
-    "Take #{it_or_one} down and pass it around"
-  end
-
-  def it_or_one
-    last_milk? ? "it" : "one"
-  end
-
-  def all_out?
-    bottles.zero?
-  end
-
-  def last_milk?
-    bottles == 1
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -3,85 +3,32 @@ class Bottles
     verses(99, 0)
   end
 
-  def verses(bottles_at_start, bottles_at_end)
-    bottles_at_start.downto(bottles_at_end).map do |bottles|
-      verse(bottles)
-    end.join("\n")
+  def verses(upper, lower)
+    upper.downto(lower).map {|i| verse(i)}.join("\n")
   end
 
-  def verse(bottles)
-    Round.new(bottles).to_s
-  end
-end
-
-class Round
-  attr_reader :bottles
-  def initialize(bottles)
-    @bottles = bottles
-  end
-
-  def to_s
-    challenge + response
-  end
-
-  def challenge
-    bottles_of_beer.capitalize + " " + on_wall + ", " +
-    bottles_of_beer + ".\n"
-  end
-
-  def response
-    go_to_the_store_or_take_one_down + ", " +
-    bottles_of_beer + " " + on_wall + ".\n"
-  end
-
-  def bottles_of_beer
-    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{beer}"
-  end
-
-  def beer
-    "beer"
-  end
-
-  def on_wall
-    "on the wall"
-  end
-
-  def pluralized_bottle_form
-    last_beer? ? "bottle" : "bottles"
-  end
-
-  def anglicized_bottle_count
-    all_out? ? "no more" : bottles.to_s
-  end
-
-  def go_to_the_store_or_take_one_down
-    if all_out?
-      @bottles = 99
-      buy_new_beer
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
     else
-      lyrics = drink_beer
-      @bottles -= 1
-      lyrics
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of beer on the wall.\n"
     end
-  end
-
-  def buy_new_beer
-    "Go to the store and buy some more"
-  end
-
-  def drink_beer
-    "Take #{it_or_one} down and pass it around"
-  end
-
-  def it_or_one
-    last_beer? ? "it" : "one"
-  end
-
-  def all_out?
-    bottles.zero?
-  end
-
-  def last_beer?
-    bottles == 1
   end
 end