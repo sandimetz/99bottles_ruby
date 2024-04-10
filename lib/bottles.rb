sandimetz/99bottles_ruby_milk

interpolate number at start of 1 case

lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     when 1
-      "1 bottle of milk on the wall, " +
+      "#{number} bottle of milk on the wall, " +
       "1 bottle of milk.\n" +
       "Take it down and pass it around, " +
       "no more bottles of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
+      "#{number} bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"