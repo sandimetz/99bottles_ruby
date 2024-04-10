sandimetz/99bottles_ruby_milk

add unneeded to_s to 1 case to make them the same

lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).capitalize} bottles of milk on the wall, " +
+      "#{quantity(number).to_s.capitalize} bottles of milk on the wall, " +
       "no more bottles of milk.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).capitalize} bottles of beer on the wall, " +
+      "#{quantity(number).to_s.capitalize} bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"