sandimetz/99bottles_ruby_milk

Add concretely abstract example

lib/bottles.rb
@@ -1,63 +1,87 @@
 class Bottles
-  NoMore = lambda do |verse|
-    "No more bottles of milk on the wall, " +
-    "no more bottles of milk.\n" +
-    "Go to the store and buy some more, " +
-    "99 bottles of milk on the wall.\n"
-  end
-
-  LastOne = lambda do |verse|
-    "1 bottle of milk on the wall, " +
-    "1 bottle of milk.\n" +
-    "Take it down and pass it around, " +
-    "no more bottles of milk on the wall.\n"
-  end
-
-  Penultimate = lambda do |verse|
-    "2 bottles of milk on the wall, " +
-    "2 bottles of milk.\n" +
-    "Take one down and pass it around, " +
-    "1 bottle of milk on the wall.\n"
-  end
-
-  Default = lambda do |verse|
-    "#{verse.number} bottles of milk on the wall, " +
-    "#{verse.number} bottles of milk.\n" +
-    "Take one down and pass it around, " +
-    "#{verse.number - 1} bottles of milk on the wall.\n"
-  end
-
   def song
     verses(99, 0)
   end
 
-  def verses(finish, start)
-    (finish).downto(start).map {|verse_number|
-      verse(verse_number) }.join("\n")
+  def verses(bottles_at_start, bottles_at_end)
+    bottles_at_start.downto(bottles_at_end).map do |bottles|
+      verse(bottles)
+    end.join("\n")
   end
 
-  def verse(number)
-    verse_for(number).text
-  end
-
-  def verse_for(number)
-    case number
-    when 0 then Verse.new(number, &NoMore)
-    when 1 then Verse.new(number, &LastOne)
-    when 2 then Verse.new(number, &Penultimate)
-    else        Verse.new(number, &Default)
-    end
+  def verse(bottles)
+    Round.new(bottles).to_s
   end
 end
 
-class Verse
-  attr_reader :number
-  def initialize(number, &lyrics)
-    @number = number
-    @lyrics = lyrics
+class Round
+  attr_reader :bottles
+  def initialize(bottles)
+    @bottles = bottles
   end
 
-  def text
-    @lyrics.call self
+  def to_s
+    challenge + response
+  end
+
+  def challenge
+    bottles_of_milk.capitalize + " " + on_wall + ", " +
+    bottles_of_milk + ".\n"
+  end
+
+  def response
+    go_to_the_store_or_take_one_down + ", " +
+    bottles_of_milk + " " + on_wall + ".\n"
+  end
+
+  def bottles_of_milk
+    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{milk}"
+  end
+
+  def milk
+    "milk"
+  end
+
+  def on_wall
+    "on the wall"
+  end
+
+  def pluralized_bottle_form
+    last_milk? ? "bottle" : "bottles"
+  end
+
+  def anglicized_bottle_count
+    all_out? ? "no more" : bottles.to_s
+  end
+
+  def go_to_the_store_or_take_one_down
+    if all_out?
+      @bottles = 99
+      buy_new_milk
+    else
+      lyrics = drink_milk
+      @bottles -= 1
+      lyrics
+    end
+  end
+
+  def buy_new_milk
+    "Go to the store and buy some more"
+  end
+
+  def drink_milk
+    "Take #{it_or_one} down and pass it around"
+  end
+
+  def it_or_one
+    last_milk? ? "it" : "one"
+  end
+
+  def all_out?
+    bottles.zero?
+  end
+
+  def last_milk?
+    bottles == 1
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,63 +1,87 @@
 class Bottles
-  NoMore = lambda do |verse|
-    "No more bottles of beer on the wall, " +
-    "no more bottles of beer.\n" +
-    "Go to the store and buy some more, " +
-    "99 bottles of beer on the wall.\n"
-  end
-
-  LastOne = lambda do |verse|
-    "1 bottle of beer on the wall, " +
-    "1 bottle of beer.\n" +
-    "Take it down and pass it around, " +
-    "no more bottles of beer on the wall.\n"
-  end
-
-  Penultimate = lambda do |verse|
-    "2 bottles of beer on the wall, " +
-    "2 bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "1 bottle of beer on the wall.\n"
-  end
-
-  Default = lambda do |verse|
-    "#{verse.number} bottles of beer on the wall, " +
-    "#{verse.number} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{verse.number - 1} bottles of beer on the wall.\n"
-  end
-
   def song
     verses(99, 0)
   end
 
-  def verses(finish, start)
-    (finish).downto(start).map {|verse_number|
-      verse(verse_number) }.join("\n")
+  def verses(bottles_at_start, bottles_at_end)
+    bottles_at_start.downto(bottles_at_end).map do |bottles|
+      verse(bottles)
+    end.join("\n")
   end
 
-  def verse(number)
-    verse_for(number).text
-  end
-
-  def verse_for(number)
-    case number
-    when 0 then Verse.new(number, &NoMore)
-    when 1 then Verse.new(number, &LastOne)
-    when 2 then Verse.new(number, &Penultimate)
-    else        Verse.new(number, &Default)
-    end
+  def verse(bottles)
+    Round.new(bottles).to_s
   end
 end
 
-class Verse
-  attr_reader :number
-  def initialize(number, &lyrics)
-    @number = number
-    @lyrics = lyrics
+class Round
+  attr_reader :bottles
+  def initialize(bottles)
+    @bottles = bottles
   end
 
-  def text
-    @lyrics.call self
+  def to_s
+    challenge + response
+  end
+
+  def challenge
+    bottles_of_beer.capitalize + " " + on_wall + ", " +
+    bottles_of_beer + ".\n"
+  end
+
+  def response
+    go_to_the_store_or_take_one_down + ", " +
+    bottles_of_beer + " " + on_wall + ".\n"
+  end
+
+  def bottles_of_beer
+    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{beer}"
+  end
+
+  def beer
+    "beer"
+  end
+
+  def on_wall
+    "on the wall"
+  end
+
+  def pluralized_bottle_form
+    last_beer? ? "bottle" : "bottles"
+  end
+
+  def anglicized_bottle_count
+    all_out? ? "no more" : bottles.to_s
+  end
+
+  def go_to_the_store_or_take_one_down
+    if all_out?
+      @bottles = 99
+      buy_new_beer
+    else
+      lyrics = drink_beer
+      @bottles -= 1
+      lyrics
+    end
+  end
+
+  def buy_new_beer
+    "Go to the store and buy some more"
+  end
+
+  def drink_beer
+    "Take #{it_or_one} down and pass it around"
+  end
+
+  def it_or_one
+    last_beer? ? "it" : "one"
+  end
+
+  def all_out?
+    bottles.zero?
+  end
+
+  def last_beer?
+    bottles == 1
   end
 end