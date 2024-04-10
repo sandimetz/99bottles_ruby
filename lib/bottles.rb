sandimetz/99bottles_ruby_milk

revert to outer conditional

lib/bottles.rb
@@ -1,9 +1,15 @@
 class Bottles
   def verse(number)
-    "#{number} bottles of milk on the wall, " +
-    "#{number} bottles of milk.\n" +
-    "Take one down and pass it around, " +
-    "#{number-1} bottle#{'s' unless (number-1) == 1} of milk " +
-      "on the wall.\n"
+    if number == 2
+      "2 bottles of milk on the wall, " +
+      "2 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of milk on the wall.\n"
+    else
+      "#{number} bottles of milk on the wall, " +
+      "#{number} bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of milk on the wall.\n"
+    end
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,9 +1,15 @@
 class Bottles
   def verse(number)
-    "#{number} bottles of beer on the wall, " +
-    "#{number} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{number-1} bottle#{'s' unless (number-1) == 1} of beer " +
-      "on the wall.\n"
+    if number == 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of beer on the wall.\n"
+    end
   end
 end