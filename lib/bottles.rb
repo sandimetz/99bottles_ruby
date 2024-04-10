sandimetz/99bottles_ruby_milk

interpolate number in 2 case

lib/bottles.rb
@@ -20,10 +20,10 @@ class Bottles
       "Take it down and pass it around, " +
       "no more bottles of milk on the wall.\n"
     when 2
-      "2 bottles of milk on the wall, " +
-      "2 bottles of milk.\n" +
+      "#{number} bottles of milk on the wall, " +
+      "#{number} bottles of milk.\n" +
       "Take one down and pass it around, " +
-      "1 bottle of milk on the wall.\n"
+      "#{number-1} bottle of milk on the wall.\n"
     else
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -20,10 +20,10 @@ class Bottles
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "#{number-1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +