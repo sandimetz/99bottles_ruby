sandimetz/99bottles_ruby_milk

Add incomprehensibly concise example

lib/bottles.rb
@@ -1,0 +1,19 @@
+class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(hi, lo)
+    hi.downto(lo).map {|n| verse(n) }.join("\n")
+  end
+
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of milk on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of milk.\n" +
+    "#{n > 0  ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+              : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of milk on the wall.\n"
+  end
+end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,0 +1,19 @@
+class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(hi, lo)
+    hi.downto(lo).map {|n| verse(n) }.join("\n")
+  end
+
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0  ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+              : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of beer on the wall.\n"
+  end
+end