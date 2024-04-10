sandimetz/99bottles_ruby_milk

send #container in phrase 1 case 1

lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     when 1
-      "#{number} bottle of milk on the wall, " +
+      "#{number} #{container(number)} of milk on the wall, " +
       "1 bottle of milk.\n" +
       "Take it down and pass it around, " +
       "no more bottles of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -15,12 +15,12 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, " +
+      "#{number} #{container(number)} of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
+      "#{number} #{container(number)} of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
       "#{number-1} #{container(number-1)} of beer on the wall.\n"