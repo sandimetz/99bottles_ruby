sandimetz/99bottles_ruby_milk

copy #verse from Bottles to BottleVerse

lib/bottles.rb
@@ -24,6 +24,15 @@ class BottleVerse
   def initialize(number)
     @number = number
   end
+
+  def verse(number)
+    bottle_number = BottleNumber.for(number)
+
+    "#{bottle_number} of milk on the wall, ".capitalize +
+    "#{bottle_number} of milk.\n" +
+    "#{bottle_number.action}, " +
+    "#{bottle_number.successor} of milk on the wall.\n"
+  end
 end
 
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -24,6 +24,15 @@ class BottleVerse
   def initialize(number)
     @number = number
   end
+
+  def verse(number)
+    bottle_number = BottleNumber.for(number)
+
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{bottle_number.successor} of beer on the wall.\n"
+  end
 end
 
 