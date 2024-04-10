sandimetz/99bottles_ruby_milk

send quantity in case 0 phrase 1

lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of milk on the wall, " +
+      "#{quantity(number).capitalize} bottles of milk on the wall, " +
       "no more bottles of milk.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "No more bottles of beer on the wall, " +
+      "#{quantity(number).capitalize} bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"