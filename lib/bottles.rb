sandimetz/99bottles_ruby_milk

use #container in 2 case

lib/bottles.rb
@@ -23,7 +23,7 @@ class Bottles
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottle of milk on the wall.\n"
+      "#{number-1} #{container(number-1)} of milk on the wall.\n"
     else
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -23,7 +23,7 @@ class Bottles
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottle of beer on the wall.\n"
+      "#{number-1} #{container(number-1)} of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +