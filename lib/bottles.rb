sandimetz/99bottles_ruby_milk

wrap long lines in verse

lib/bottles.rb
@@ -8,10 +8,12 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
+    "#{quantity(number).capitalize} #{container(number)} " +
+      "of milk on the wall, " +
     "#{quantity(number)} #{container(number)} of milk.\n" +
     "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
+    "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of milk on the wall.\n"
   end
 
   def quantity(number)

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -8,10 +8,12 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number).capitalize} #{container(number)} " +
+      "of beer on the wall, " +
     "#{quantity(number)} #{container(number)} of beer.\n" +
     "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of beer on the wall.\n"
   end
 
   def quantity(number)