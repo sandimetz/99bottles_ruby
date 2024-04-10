sandimetz/99bottles_ruby_milk

send quantity in else phrase 2

lib/bottles.rb
@@ -16,7 +16,7 @@ class Bottles
       "99 bottles of milk on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
-      "#{number} #{container(number)} of milk.\n" +
+      "#{quantity(number)} #{container(number)} of milk.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of milk on the wall.\n"
     end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -16,7 +16,7 @@ class Bottles
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end