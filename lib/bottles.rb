sandimetz/99bottles_ruby_milk

interpolate number into verse string

lib/bottles.rb
@@ -1,14 +1,8 @@
 class Bottles
   def verse(number)
-    if number == 99
-      n = 99
-    else
-      n = 3
-    end
-
-    "#{n} bottles of milk on the wall, " +
-    "#{n} bottles of milk.\n" +
+    "#{number} bottles of milk on the wall, " +
+    "#{number} bottles of milk.\n" +
     "Take one down and pass it around, " +
-    "#{n - 1} bottles of milk on the wall.\n"
+    "#{number-1} bottles of milk on the wall.\n"
   end
 end

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -1,14 +1,8 @@
 class Bottles
   def verse(number)
-    if number == 99
-      n = 99
-    else
-      n = 3
-    end
-
-    "#{n} bottles of beer on the wall, " +
-    "#{n} bottles of beer.\n" +
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
     "Take one down and pass it around, " +
-    "#{n - 1} bottles of beer on the wall.\n"
+    "#{number-1} bottles of beer on the wall.\n"
   end
 end