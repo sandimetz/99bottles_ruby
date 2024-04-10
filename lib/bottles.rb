sandimetz/99bottles_ruby_milk

use bottle_number is phrase 2

lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} " +
       "of milk on the wall, " +
-    "#{quantity(number)} #{container(number)} of milk.\n" +
+    "#{bottle_number.quantity} #{bottle_number.container} of milk.\n" +
     "#{bottle_number.action}, " +
     "#{quantity(successor(number))} #{container(successor(number))} " +
       "of milk on the wall.\n"

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} " +
       "of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{quantity(successor(number))} #{container(successor(number))} " +
       "of beer on the wall.\n"