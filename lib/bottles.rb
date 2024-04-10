sandimetz/99bottles_ruby_milk

send #container in default

lib/bottles.rb
@@ -28,7 +28,7 @@ class Bottles
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottles of milk on the wall.\n"
+      "#{number-1} #{container} of milk on the wall.\n"
     end
   end
 

sandimetz/99bottles_ruby

lib/bottles.rb
@@ -28,7 +28,7 @@ class Bottles
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
+      "#{number-1} #{container} of beer on the wall.\n"
     end
   end
 