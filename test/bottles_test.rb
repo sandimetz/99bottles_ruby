sandimetz/99bottles_ruby_milk

copy all other verse tests into BottleVerseTest

test/bottles_test.rb
@@ -12,6 +12,42 @@ class BottleVerseTest < Minitest::Test
       "98 bottles of milk on the wall.\n"
     assert_equal expected, BottleVerse.lyrics(99)
   end
+
+  def test_another_verse
+    expected =
+      "3 bottles of milk on the wall, " +
+      "3 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(3)
+  end
+
+  def test_verse_2
+    expected =
+      "2 bottles of milk on the wall, " +
+      "2 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected =
+      "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n"
+     assert_equal expected, Bottles.new.verse(1)
+  end
+
+  def test_verse_0
+    expected =
+      "No more bottles of milk on the wall, " +
+      "no more bottles of milk.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(0)
+  end
 end
 
 class BottlesTest < Minitest::Test

sandimetz/99bottles_ruby

test/bottles_test.rb
@@ -12,6 +12,42 @@ class BottleVerseTest < Minitest::Test
       "98 bottles of beer on the wall.\n"
     assert_equal expected, BottleVerse.lyrics(99)
   end
+
+  def test_another_verse
+    expected =
+      "3 bottles of beer on the wall, " +
+      "3 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(3)
+  end
+
+  def test_verse_2
+    expected =
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected =
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+     assert_equal expected, Bottles.new.verse(1)
+  end
+
+  def test_verse_0
+    expected =
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(0)
+  end
 end
 
 class BottlesTest < Minitest::Test